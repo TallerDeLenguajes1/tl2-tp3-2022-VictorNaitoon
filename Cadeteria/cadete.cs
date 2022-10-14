da99cf505716dc21b8bc71402fcb2f4b4d5415dc
Victor Molina <Victormolinalvp@gmail.com>
2022-10-14 01:52:12 -0300

    clase cadete lista

---
+++ Cadeteria/cadete.cs
@@ -0,0 +1,27 @@
+public class Cadete : Persona {
+    List<Pedidos>? listadoDePedidos;
+    private float TotalACobrar=0;
+
+    public List<Pedidos>? ListadoDePedidos { get => listadoDePedidos; set => listadoDePedidos = value; }
+
+    public Cadete(string ID, string Nombre, string Direccion, string Telefono) : base(ID, Nombre, Direccion, Telefono){
+        this.ListadoDePedidos = new List<Pedidos>();
+    }
+
+    public void AgregarPedido(Pedidos nuevoPedido){
+        this.ListadoDePedidos.Add(nuevoPedido);
+    }
+
+    public float JornalACobrar(){
+        foreach (var item in this.ListadoDePedidos)
+        {
+            if (item.Estado == "Entregado" || item.Estado == "entregado")
+            {
+                this.TotalACobrar += 300;
+            } else {
+                this.TotalACobrar += 0;
+            }
+        }
+        return TotalACobrar;
+    }
+}
